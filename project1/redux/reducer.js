8a2dfda588c8f8650c7992fac3b5638483766cf5
seen2 <tiwarietc@gmail.com>
2018-08-27 19:34:18 +0530

    working with redux

--- project1/redux/reducer.js
+++ project1/redux/reducer.js
@@ -1,12 +1,35 @@
+//action types
+const post='POST'
+const timeline='TIMELINE'
+
+//action creators
+const createPost=()=>({
+	type:post,
+	payload:{
+		likes:0,
+		comments:{},
+	}
+});
+
+const createTimelne=()=>({
+	type:timeline,
+	payload:{
+
+	},
+});
+
+
 //reducer for updating post
+const postReducer=(state,action)=>{
+	switch(action.type==post){
+
+	}
 	//reducer for updating likes
 	//reducer for updating comments
 	//reducer for updating texts
 
 //reducer for updating timeline
 
-//action types
-//action creators
 
 
 //combine reducers
