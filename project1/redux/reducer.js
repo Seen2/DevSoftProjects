be13c0b4db9910d9399b6f97d4383e8c497bda6a
seen2 <tiwarietc@gmail.com>
2018-08-27 09:39:25 +0530

    redux started

---
+++ project1/redux/reducer.js
@@ -0,0 +1,14 @@
+//reducer for updating post
+	//reducer for updating likes
+	//reducer for updating comments
+	//reducer for updating texts
+
+//reducer for updating timeline
+
+//action types
+//action creators
+
+
+//combine reducers
+
+//createStore
